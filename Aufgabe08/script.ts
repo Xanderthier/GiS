e7b65fbb419c052e2088afc512a0e667733b579d
Xanderthier <Thier32@gmx.de>
2020-06-21 16:42:00 +0200

    Aufgabe08.1

---
+++ Aufgabe08/script.ts
@@ -0,0 +1,21 @@
+namespace Aufgabe08 {
+
+
+    let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
+    button.addEventListener("click", communicate);
+
+    async function communicate(): Promise<void> {
+        let formData: FormData = new FormData(document.forms[0]);
+        let url: string = "https://gis20sw.herokuapp.com/";
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url = url + "?" + query.toString();
+        await fetch(url);
+
+        for (let entry of query) {
+            console.log(entry);
+            console.log("name: " + entry[0]);
+            console.log("value: " + entry[1]);
+        }
+    }
+
+}
