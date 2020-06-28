e3a3c3bea7a22cc0489753e397249cec0cc3efb0
Xanderthier <Thier32@gmx.de>
2020-06-28 16:55:44 +0200

    server changes

--- Aufgabe09/testo.js
+++ Aufgabe09/testo.js
@@ -2,34 +2,38 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Aufgabe09 = void 0;
 const Http = require("http");
-// export namespace um für allgemeinheit zugriff zu gewähren/Sichtbar zu machen
+const url = require("url");
 var Aufgabe09;
 (function (Aufgabe09) {
-    //Console log gibt starting server aus
     console.log("Starting server");
-    //Port wird festgelegt
     let port = Number(process.env.PORT);
-    //Wenn port nicht richtig wird Port auf richtigen Port zugewiesen
     if (!port)
         port = 8100;
-    //"erstelle server"
     let server = Http.createServer();
-    //fragt den port, request und listener ab
     server.addListener("request", handleRequest);
     server.addListener("listening", handleListen);
     server.listen(port);
-    //gibt "listening" aus wenn handleListen() aktiv ist
     function handleListen() {
         console.log("Listening");
     }
-    //gibt "I hear voices!" aus wenn handleRequest aufgerufen wird. Als parameter Request und Response.
     function handleRequest(_request, _response) {
         console.log("I hear voices!");
-        //baut html seite auf
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write(_request.url);
-        _response.end();
+        if (_request.url) {
+            let q = url.parse(_request.url, true);
+            let pfad = q.pathname;
+            if (pfad == "/html") {
+                for (let key in q.query) {
+                    _response.write(key + ": " + q.query[key] + "<br/>");
+                }
+            }
+            if (pfad == "/json") {
+                let jsonString = JSON.stringify(q.query);
+                _response.write(jsonString);
+            }
+            _response.end();
+        }
     }
 })(Aufgabe09 = exports.Aufgabe09 || (exports.Aufgabe09 = {}));
 //# sourceMappingURL=testo.js.map
--- Aufgabe09/testo.ts
+++ Aufgabe09/testo.ts
@@ -1,38 +1,44 @@
-  
 import * as Http from "http";
+import * as url from "url";
 
-// export namespace um für allgemeinheit zugriff zu gewähren/Sichtbar zu machen
 export namespace Aufgabe09 {
-  //Console log gibt starting server aus
   console.log("Starting server");
-  //Port wird festgelegt
   let port: number = Number(process.env.PORT);
-  //Wenn port nicht richtig wird Port auf richtigen Port zugewiesen
   if (!port)
     port = 8100;
 
-    //"erstelle server"
   let server: Http.Server = Http.createServer();
-  //fragt den port, request und listener ab
   server.addListener("request", handleRequest);
   server.addListener("listening", handleListen);
   server.listen(port);
 
-  //gibt "listening" aus wenn handleListen() aktiv ist
+
   function handleListen(): void {
     console.log("Listening");
   }
 
-  //gibt "I hear voices!" aus wenn handleRequest aufgerufen wird. Als parameter Request und Response.
   function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
     console.log("I hear voices!");
 
-    //baut html seite auf
     _response.setHeader("content-type", "text/html; charset=utf-8");
     _response.setHeader("Access-Control-Allow-Origin", "*");
 
-    _response.write(_request.url);
+    if (_request.url) {
+      let q: url.UrlWithParsedQuery = url.parse(_request.url, true);
+      let pfad: string | null = q.pathname;
 
-    _response.end();
+      if (pfad == "/html") {
+        for (let key in q.query) {
+          _response.write(key + ": " + q.query[key] + "<br/>");
+
+        }
+      }
+      
+      if (pfad == "/json") {
+        let jsonString: string = JSON.stringify(q.query);
+        _response.write(jsonString);
+      }
+      _response.end();
+    }
   }
-}
+  }
