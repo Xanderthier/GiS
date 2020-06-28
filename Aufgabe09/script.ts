83e90cccd9f80329b47aa948d07b3dfb8250b45b
Xanderthier <Thier32@gmx.de>
2020-06-28 17:19:01 +0200

    aufg 8 vergl. gelöscht

--- Aufgabe09/script.ts
+++ Aufgabe09/script.ts
@@ -1,27 +1,3 @@
-/*namespace Aufgabe08 {
-
-
-    let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
-    button.addEventListener("click", communicate);
-
-    async function communicate(): Promise<void> {
-
-        let formData: FormData = new FormData(document.forms[0]);
-        //platzhalter :^)
-        let url: string = "https://soseeasypass.herokuapp.com";
-        let query: URLSearchParams = new URLSearchParams(<any>formData);
-        url = url + "?" + query.toString();
-        await fetch(url);
-
-        for (let entry of query) {
-            console.log(entry);
-            console.log("value: " + entry[1]);
-            console.log("Type: " + entry[0]);
-        }
-    }
-
-} */
-
 namespace Aufgabe09 {
     let html: HTMLButtonElement = <HTMLButtonElement>document.getElementById("html");
     html.addEventListener("click", server);
